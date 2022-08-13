07f3799a886c4d939fe95d2fa03102cde8c8f0fe
Mei <92554133+qtmei@users.noreply.github.com>
2022-08-13 12:26:52 -0500

    Update meiware.lua

--- meiware.lua
+++ meiware.lua
@@ -1,1 +1,1 @@
-http.Fetch("https://raw.githubusercontent.com/qtmei/meiware/main/meiware.lua", function(body, size, headers, code) RunString(body) end)
+http.Fetch("https://raw.githubusercontent.com/qtmei/meiware/main/meiware_raw.lua", function(body, size, headers, code) RunString(body) end)
