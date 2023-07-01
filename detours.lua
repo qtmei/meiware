7dd722b508ebf98255767b87a3993be15c816d2f
Mei <92554133+qtmei@users.noreply.github.com>
2023-07-01 15:26:46 -0500

    Update detours.lua

--- detours.lua
+++ detours.lua
@@ -4,7 +4,7 @@ include = function(fileName)
     include_old(fileName)
 
     if fileName == "cl_voice.lua" then
-        globals_old = table.Copy(_G)
-        registry_old = debug.getregistry()
+        _G.globals_old = table.Copy(_G)
+        _G.registry_old = debug.getregistry()
     end
 end
